70b37b1d22a517010ce05169fd70fcff5b35048c
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2022-04-07 15:24:47 +0300

    Added dark mode option

--- app/src/main/java/com/msk/moviesapplication/Util/Constants.kt
+++ app/src/main/java/com/msk/moviesapplication/Util/Constants.kt
@@ -3,4 +3,5 @@ package com.msk.moviesapplication.Util
 object Constants {
     const val URL="https://api.themoviedb.org/3/"
     const val API_KEY="db1749f984b02273c93fce0b07cc3686"
+    const val IMAGEBASEURl="https://image.tmdb.org/t/p/w500"
 }
--- app/src/main/java/com/msk/moviesapplication/ui/MainActivity/MainActivity.kt
+++ app/src/main/java/com/msk/moviesapplication/ui/MainActivity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.msk.moviesapplication.ui.MainActivity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -10,10 +11,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.rememberNavController
 import com.msk.moviesapplication.api.api
+import com.msk.moviesapplication.ui.movies.MoviesScreen
 import com.msk.moviesapplication.ui.theme.MoviesApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -26,19 +32,14 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var api:api
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MoviesApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                  lifecycleScope.launch{
-                     val a= api.getGenre().genres[0].name
-                      Log.d("adana",a)
-                  }
-                }
+            val darktheme:MutableState<Boolean> = remember { mutableStateOf(true) }
+            val navController= rememberNavController()
+            MoviesApplicationTheme(darkTheme =darktheme.value ) {
+               MoviesScreen(navController,darktheme)
             }
         }
     }
